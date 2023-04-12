5b2de3d0ad5e4c0f23ced38e48ca0a05450f973e
Leviathan <1015295213@qq.com>
2023-04-12 14:22:12 +0800

    精简代码，修改显示样式

--- demo.go
+++ demo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/Leviathangk/go-glog/glog"
+	"os"
 )
 
 func main() {
@@ -18,21 +19,21 @@ func main() {
 	logger.Config.PrintLevel = glog.TraceLevel  // 默认是 debug 级别，这里修改
 	logger.Config.OutPutLevel = glog.TraceLevel // 默认是 debug 级别，这里修改
 
-	//// AddOutPut
-	//file, _ := os.OpenFile("demo.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	//logger.AddOutPut(file)
-	//
-	//// AddHook：实现分类处理
-	//errFile, _ := os.OpenFile("err.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	//
-	//logger.AddHook(func(level int, out string) {
-	//	if level == glog.ErrorLevel {
-	//		_, err := errFile.Write([]byte(out))
-	//		if err != nil {
-	//			return
-	//		}
-	//	}
-	//})
+	// AddOutPut
+	file, _ := os.OpenFile("demo.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logger.AddOutPut(file)
+
+	// AddHook：实现分类处理
+	errFile, _ := os.OpenFile("err.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+
+	logger.AddHook(func(level int, out string) {
+		if level == glog.ErrorLevel {
+			_, err := errFile.Write([]byte(out))
+			if err != nil {
+				return
+			}
+		}
+	})
 
 	// 正常使用：默认 logger 可以直接使用 glog.Debugln()
 	logger.Traceln("Trace")
